fc86a8bf26a4a4d89cd286b60cfab222d20de597
žWındßłade <ga.rojasa@outlook.com>
2023-04-12 13:31:04 -0400

    First commit.

---
+++ src/test/java/cl/afterlife/forbiddance/AfterlifeForbiddanceApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.afterlife.forbiddance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AfterlifeForbiddanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
